bd2d2d80a021c149932f7529593ffdf7e5e76d4f
Alexey_Vo <Byk8888@mail.ru>
2017-08-06 20:47:18 +0300

    Prepare to HW0

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,12 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * GKislin
@@ -23,13 +24,28 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        List<UserMealWithExceed> filteredWithExceeded = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO return filtered list with correctly exceeded field
-        return null;
+        List<UserMealWithExceed> list ;
+        Map<LocalDate, Integer> map = new HashMap<>();
+
+        for (UserMeal u :
+                mealList) {
+            LocalDate date = u.getDateTime().toLocalDate();
+            if(map.containsKey(date)) map.put(date, map.get(date)+u.getCalories());
+            else map.put(date, u.getCalories());
+        }
+
+        list = mealList.stream()
+                .filter(o -> TimeUtil.isBetween(o.getDateTime().toLocalTime(), startTime, endTime))
+                .map(o -> new UserMealWithExceed(o.getDateTime(), o.getDescription(), o.getCalories(), map.get(o.getDateTime().toLocalDate())>caloriesPerDay))
+                .collect(Collectors.toList());
+
+        return list;
     }
 }
